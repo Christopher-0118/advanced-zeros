1210fa3ca9f7be1ab2da8b38fec19f1f07ea16df
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-05 18:09:04 +0300

    advanced-zeros/feature: implement getZerosCount method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxDegree = Math.floor(Math.log(number) / Math.log(base));
+  let zerosArr = [];
+  let currentNum = base;
+  let zerosCount = 0;
+
+  for( let currentDegree = 1; currentDegree <= maxDegree; currentDegree++) {
+    zerosCount +=  Math.floor(number / currentNum);
+    currentNum = currentNum * base;
+  }
+  return zerosCount;
 }
